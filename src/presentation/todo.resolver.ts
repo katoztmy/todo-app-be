a93739a1877819f7e2b60e4b7a1ac7a9ea992ed5
katoztmy <kato.eng.dao@gmail.com>
2025-12-09 00:38:00 +0900

    fix

--- src/presentation/todo.resolver.ts
+++ src/presentation/todo.resolver.ts
@@ -1,24 +1,59 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
-import { Todo, UpdateTodoInput } from 'src/todo/models/todo.model';
-import { TodoRepository } from 'src/infra/database/todo/todo.repository';
-import { CreateTodoInput } from 'src/todo/models/todo.model';
+import { Inject } from '@nestjs/common';
+import {
+  Todo,
+  CreateTodoInput,
+  UpdateTodoInput,
+} from 'src/todo/models/todo.model';
+import { ITodoRepository } from 'src/infra/database/todo/interface';
+import { Todo as TodoEntity } from 'src/todo/entity/todo.entity';
 
 @Resolver(() => Todo)
 export class TodoResolver {
-  constructor(private readonly todoRepository: TodoRepository) {}
+  constructor(
+    @Inject('ITodoRepository')
+    private readonly todoRepository: ITodoRepository,
+  ) {}
 
   @Query(() => [Todo])
   async todos(): Promise<Todo[]> {
-    return await this.todoRepository.findAll();
+    const todos: TodoEntity[] = await this.todoRepository.findAll();
+    return todos.map((todo) => ({
+      id: todo.id,
+      title: todo.title,
+      description: todo.description,
+      dueDate: todo.dueDate,
+      completed: todo.completed,
+      createdAt: todo.createdAt,
+      updatedAt: todo.updatedAt,
+    }));
   }
 
   @Mutation(() => Todo)
-  async createTodo(@Args('input') input: CreateTodoInput) {
-    return await this.todoRepository.create(input);
+  async createTodo(@Args('input') input: CreateTodoInput): Promise<Todo> {
+    const todo: TodoEntity = await this.todoRepository.create(input);
+    return {
+      id: todo.id,
+      title: todo.title,
+      description: todo.description,
+      dueDate: todo.dueDate,
+      completed: todo.completed,
+      createdAt: todo.createdAt,
+      updatedAt: todo.updatedAt,
+    };
   }
 
   @Mutation(() => Todo)
-  async updateTodo(@Args('input') input: UpdateTodoInput) {
-    return await this.todoRepository.update(input);
+  async updateTodo(@Args('input') input: UpdateTodoInput): Promise<Todo> {
+    const todo: TodoEntity = await this.todoRepository.update(input);
+    return {
+      id: todo.id,
+      title: todo.title,
+      description: todo.description,
+      dueDate: todo.dueDate,
+      completed: todo.completed,
+      createdAt: todo.createdAt,
+      updatedAt: todo.updatedAt,
+    };
   }
 }
